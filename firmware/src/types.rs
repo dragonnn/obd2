098d47b44efd64512837279d3530bc8c9c90108b
dragonn <dragonn@op.pl>
2024-09-03 10:35:39 +0200

    simplify types

--- firmware/src/types.rs
+++ firmware/src/types.rs
@@ -1,3 +1,4 @@
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
 use esp_hal::{gpio::*, spi::FullDuplexMode, Async};
 
@@ -15,35 +16,20 @@ pub type Spi = Mutex<
 pub use crate::hal::SpiBus;
 
 pub type Mcp2515 = crate::mcp2515::Mcp2515<
-    embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig<
-        'static,
-        CriticalSectionRawMutex,
-        SpiBus,
-        Output<'static, GpioPin<17>>,
-    >,
+    SpiDeviceWithConfig<'static, CriticalSectionRawMutex, SpiBus, Output<'static, GpioPin<17>>>,
     Input<'static, GpioPin<4>>,
 >;
 
 pub type Sh1122<const CS: u8> = sh1122::AsyncDisplay<
     display_interface_spi::SPIInterface<
-        embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig<
-            'static,
-            CriticalSectionRawMutex,
-            SpiBus,
-            Output<'static, GpioPin<CS>>,
-        >,
+        SpiDeviceWithConfig<'static, CriticalSectionRawMutex, SpiBus, Output<'static, GpioPin<CS>>>,
         Output<'static, GpioPin<23>>,
     >,
     sh1122::async_display::buffered_graphics::AsyncBufferedGraphicsMode,
 >;
 
 pub type Cap1188 = crate::cap1188::Cap1188<
-    embassy_embedded_hal::shared_bus::asynch::spi::SpiDeviceWithConfig<
-        'static,
-        CriticalSectionRawMutex,
-        SpiBus,
-        Output<'static, GpioPin<20>>,
-    >,
+    SpiDeviceWithConfig<'static, CriticalSectionRawMutex, SpiBus, Output<'static, GpioPin<20>>>,
     Input<'static, GpioPin<3>>,
 >;
 
