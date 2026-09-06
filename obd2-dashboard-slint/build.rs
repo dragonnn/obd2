ce5cf2407be23a00609ba1adf11337c6df882177
dragonn <dragonn@op.pl>
2026-09-06 17:30:09 +0200

    more stuff

--- obd2-dashboard-slint/build.rs
+++ obd2-dashboard-slint/build.rs
@@ -1,3 +1,15 @@
+use std::{collections::HashMap, path::PathBuf};
+
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    let library = HashMap::from([
+        ("lucide".to_string(), PathBuf::from(lucide_slint::lib())),
+        (
+            // If you are not going to use Lucide Lab, remove this tuple
+            "lucide-lab".to_string(),
+            PathBuf::from(lucide_slint::lib_lab()),
+        ),
+    ]);
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
+
+    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
 }
